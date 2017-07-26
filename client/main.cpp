3df7365b35e9d03be674d862e49f543da0931d2a
hulk006 <yanghaohulk@foxmail.com>
2017-07-26 10:59:17 +0800

    add the client input params for debug api

--- client/main.cpp
+++ client/main.cpp
@@ -11,60 +11,65 @@
 
 
 #define PORT 2245
-int main() {
+int main(int argc ,char **argv )
+{
 
+    int sockfd;//客户端的socket
+    char buffer[2014];
 
-    while (1) {
+    struct sockaddr_in server_addr;
+    struct hostent *host;
 
-        int sockfd;
-        char buffer[2014];
-
-        struct sockaddr_in server_addr;
-        struct hostent *host;
-        int nbytes;
-        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-            fprintf(stderr, "Socket Error is %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-        bzero(&server_addr, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(PORT);
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        char sendbuf[1024]= "";
-        char recvbuf[2014];
-
-        //客户端发出请求
-        if (connect(sockfd, (struct sockaddr *) (&server_addr), sizeof(struct sockaddr)) == -1) {
-            fprintf(stderr, "Connect failed\n");
-            exit(EXIT_FAILURE);
-        }
-
-        char ai_input[] = "poker_info;111111;hole,2H,QC;com_card,;current_bet,100; game_pool_,150;chip,4000;check_,0;connect,0;send_end;";
-        //char ai_input[] = "";
-        //fgets(ai_input, sizeof(ai_input), stdin);
-        strcpy( sendbuf, ai_input);
-        send(sockfd, sendbuf, strlen(sendbuf), 0);
-        std::cout<<"sendbuf="<<sendbuf<<std::endl;
-
-        if (strcmp(ai_input, "exit\n") == 0)
-            break;
-        recvbuf;
-        std::cout<<"recvbuf1="<<recvbuf<<std::endl;
-        int recv_flag = recv(sockfd, recvbuf, sizeof(recvbuf), 0);
-        if(recv_flag >=0 )
-        {
-            std::cout<<"recvbuf="<<recvbuf<<std::endl;
-            memset(sendbuf, 0, sizeof(sendbuf));
-            memset(recvbuf, 0, sizeof(recvbuf));
-            sleep(1);
-
-        } else
-        {
-            continue;
-        }
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)//如果没有建立socket，就错误退出
+    {
+        fprintf(stderr, "Socket Error is %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
     }
+    //设置server_addr的值，服务器的信息
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //定义发送字符，接受字符
+    char sendbuf[1024]= "";
+    char recvbuf[2014];
+    memset(recvbuf, 0, sizeof(recvbuf));
+
+    //客户端发出请求，尝试与服务器建立链接，如果失败就报错
+    if (connect(sockfd, (struct sockaddr *) (&server_addr), sizeof(struct sockaddr)) == -1)
+    {
+        fprintf(stderr, "Connect failed\n");
+        exit(EXIT_FAILURE);
+    }
+    char *ai_input;
+    if(argc == 1)
+    {
+        ai_input =  "poker_info;111111;hole,2H,QC;com_card,;current_bet,100; game_pool_,150;chip,4000;check_,0;connect,0;send_end;";
+    }
+    else if(argc == 2)
+    {
+        ai_input = argv[1];
+    }
+    else
+    {
+        fprintf(stderr,"error:input param id mismatched!\n");//错误输出
+        exit(EXIT_FAILURE);
+    }
+    //赋值字符串
+    strcpy(sendbuf, ai_input);
+    send(sockfd, sendbuf, strlen(sendbuf), 0);
+    std::cout<<"sendbuf="<<sendbuf<<std::endl;
+    int recv_flag = recv(sockfd, recvbuf, sizeof(recvbuf), 0);//接受服务器发送来的信息存入recvbuf
+    if(recv_flag >=0 )
+    {
+        std::cout<<"recvbuf="<<recvbuf<<std::endl;
+    }
+    else
+    {
+        fprintf(stderr,"the server is not working");
+    }
+
     //close(sockfd);
-    exit(EXIT_SUCCESS);
     return 0;
 
 }
--- test_card/main.cpp
+++ test_card/main.cpp
@@ -1,55 +1,12 @@
 #include <iostream>
 
-int main() {
-
-    std::string card_string = "QD";
-    char value_raw = card_string[0];//
-    char suit_raw = card_string[1];//
-    char *t="T",*j="J",*q="Q",*k="K",*a="A";
-    int T=(int)*t,J=(int)*j,Q=(int)*q,K=(int)*k,A=(int)*a;
-    int value;
-    switch ((int)value_raw)
-    {
-        case 84:
-            value = 10-2;
-            break;
-        case 74:
-             value = 11-2;
-           break;
-        case 81:
-             value = 12-2;
-           break;
-        case 75:
-             value = 13-2;
-          break;
-        case 65:
-             value = 14-2;
-          break;
-        default:
-               value = (int) card_string[0]-50 ;
-            break;
-    }
-    int te = 574&7936;
- int card[7] = {0,4,4,5,5,6,8};
-        int res = 0;
-        for (int i = 0; i < 7; ++i)
-        {
-            res *= 10;
-            res += card[i];
-        }
-    int card1[7] = {0,4,4,5,5,6,8};
-    int res1 = 0;
-    for (int i = 0; i < 7; ++i)
-    {
-        res1 *= 10;
-        res1 += card1[i];
-    }
-
-
-    //T;
-
-
-
+int main(int argc,char **argv)
+{
     std::cout << "Hello, World!" << std::endl;
+    for(int i = 0;i<argc;++i)
+    {
+        std::cout<<argc<<" "<<"input param is:"<<argv[i]<<std::endl;
+    }
+
     return 0;
 }
